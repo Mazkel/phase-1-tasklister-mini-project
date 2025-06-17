c565f09d95d6bc568789abc263963aa9e2080e63
Silongo Vailolo <silongo68@hotmail.com>
2025-06-17 16:15:20 +1000

    Done tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const tasks = document.querySelector('#tasks');
+    const input = document.querySelector('#new-task-description');
+    const form = document.getElementById("create-task-form");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+    const taskText = input.value;
+    const priority = document.getElementById('priority').value;
+
+    const lists = document.createElement('li');
+    lists.textContent = taskText ;
+
+    if (priority === 'high') {
+      lists.style.color = 'red';
+      } else if (priority === 'medium') {
+        lists.style.color = 'orange';
+      } else {lists.style.color = 'green'};
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = '❌';
+    deleteBtn.style.cursor = 'pointer'
+    deleteBtn.addEventListener('click', () => {
+      lists.remove();
+    });
+
+    lists.appendChild(deleteBtn)
+    tasks.appendChild(lists)
+    form.reset();
+  });
 });
+
